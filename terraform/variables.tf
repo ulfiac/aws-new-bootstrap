f8d12bb89a91e0fb4a6c4371333362cb145ca701
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-11-17 21:29:30 -0500

    second refactor

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,13 +7,3 @@ variable "oidc_role_to_assume" {
   description = "The name of the IAM role to be assumed by OIDC-auth'ed Github Actions."
   type        = string
 }
-
-variable "oidc_provider_hostname" {
-  description = "The hostname of the OIDC provider for Github Actions."
-  type        = string
-}
-
-variable "tf_state_s3_bucket_name" {
-  description = "The name of the s3 bucket for terraform state(s)."
-  type        = string
-}
