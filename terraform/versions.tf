9079e1b182f0a164ee9e4e4b46e4f756f47b9f3f
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-12-03 20:23:58 -0500

    set versions to float on latest

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.21.0"
+      version = ">= 6.24.0" # current as of 2025-12-03
     }
   }
-  required_version = ">= 1.13.5"
+  required_version = ">= 1.14.1" # current as of 2025-12-03
 }
