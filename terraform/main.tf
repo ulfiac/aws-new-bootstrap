01e9f27d1ff3dd14fbe8b58f0cb5e9115cc88902
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-12-05 21:31:59 -0500

    refactor aws account id and region

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,8 @@
 locals {
-  account_id = data.aws_caller_identity.current.account_id
+  aws_account_id = data.aws_caller_identity.current.account_id
 
   # OIDC provider hostname is a stable constant defined by GitHub and AWS specifications; hardcoded for security and stability reasons
   oidc_provider_hostname = "token.actions.githubusercontent.com"
+
+  terraform_state_bucket_name = "terraform-state-${local.aws_account_id}-${var.aws_region}"
 }
