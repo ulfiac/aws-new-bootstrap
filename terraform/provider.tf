8edbfbe33f146a2fb1c58071d8e6d92ba78413ed
ulfiac <221777317+ulfiac@users.noreply.github.com>
2026-01-06 22:12:59 -0500

    fix tags module source

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 module "tags" {
   # tflint-ignore: terraform_module_pinned_source
-  source  = "git::https://github.com/ulfiac/terraform-infrastructure-modules.git//modules/tags?ref=main"
+  source  = "git::https://github.com/ulfiac/infra.git//terragrunt/_modules/tags?ref=main"
   project = "aws-bootstrap"
 }
 
